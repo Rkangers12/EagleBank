894aa068f5dcd4b6b24e4c6634353d5a83e1ae37
Ranveer Kang <case.ran.k93@outlook.com>
2025-11-11 16:43:07 +0000

    address mapper test

---
+++ src/test/java/com/studio/eaglebank/services/mappers/AddressMapperTest.java
@@ -0,0 +1,64 @@
+package com.studio.eaglebank.services.mappers;
+
+import com.studio.eaglebank.domain.entities.AddressEntity;
+import com.studio.eaglebank.domain.models.Address;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressMapperTest {
+
+    private final AddressMapper unit = new AddressMapper();
+
+    @Test
+    void mapAddressToAddressEntity() {
+
+        // Given
+        Address address = getAddress();
+        AddressEntity expected = getAddressEntity();
+
+        // When
+        AddressEntity actual = unit.mapAddressToAddressEntity(address);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void mapAddressEntityToAddress() {
+
+        // Given
+        Address expected = getAddress();
+        AddressEntity addressEntity = getAddressEntity();
+
+        // When
+        Address actual = unit.mapAddressEntityToAddress(addressEntity);
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private Address getAddress() {
+        return new Address(
+                "23 Maple Crescent",
+                "Flat 4B",
+                "",
+                "Nottingham",
+                "Nottinghamshire",
+                "NG1 3EZ"
+        );
+    }
+
+    private AddressEntity getAddressEntity() {
+        return AddressEntity.builder()
+                .line1("23 Maple Crescent")
+                .line2("Flat 4B")
+                .line3("")
+                .town("Nottingham")
+                .county("Nottinghamshire")
+                .postcode("NG1 3EZ")
+                .build();
+    }
+}
