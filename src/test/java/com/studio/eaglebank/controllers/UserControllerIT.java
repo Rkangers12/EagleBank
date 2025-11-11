35706f19a44c5ef369a07a5b09677e3d40ae99b6
Ranveer Kang <case.ran.k93@outlook.com>
2025-11-11 20:16:38 +0000

    Add integration test for user controller creation of user

--- src/test/java/com/studio/eaglebank/controllers/UserControllerIT.java
+++ src/test/java/com/studio/eaglebank/controllers/UserControllerIT.java
@@ -2,45 +2,56 @@ package com.studio.eaglebank.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studio.eaglebank.config.GlobalExceptionHandler;
+import com.studio.eaglebank.domain.entities.AddressEntity;
+import com.studio.eaglebank.domain.entities.UserEntity;
 import com.studio.eaglebank.domain.models.Address;
+import com.studio.eaglebank.domain.repositories.UserRepository;
 import com.studio.eaglebank.domain.requests.CreateUserRequest;
 import com.studio.eaglebank.domain.responses.UserResponse;
-import com.studio.eaglebank.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Optional;
+
 import static com.studio.eaglebank.services.mappers.AddressMapperTest.getAddress;
+import static com.studio.eaglebank.services.mappers.AddressMapperTest.getAddressEntity;
 import static com.studio.eaglebank.services.mappers.UserMapperTest.getCreateUserRequest;
+import static com.studio.eaglebank.services.mappers.UserMapperTest.getUserEntity;
 import static com.studio.eaglebank.services.mappers.UserMapperTest.getUserResponse;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.contentOf;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
-class UserControllerTest {
+@SpringBootTest
+@ActiveProfiles("test")
+public class UserControllerIT {
 
     public MockMvc mvc;
 
-    private ObjectMapper objectMapper;
+    @Autowired
+    private UserRepository userRepository;
 
-    @Mock
-    private UserService userServiceMock;
+    @Autowired UserController userController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp() {
-        UserController unit = new UserController(userServiceMock);
-        mvc = MockMvcBuilders.standaloneSetup(unit)
+        mvc = MockMvcBuilders.standaloneSetup(userController)
                 .setControllerAdvice(new GlobalExceptionHandler())
                 .build();
-        objectMapper = new ObjectMapper();
     }
 
     @Test
@@ -50,10 +61,6 @@ class UserControllerTest {
         Address address = getAddress();
         CreateUserRequest createUserRequest = getCreateUserRequest(address);
 
-        UserResponse expected = getUserResponse(address);
-
-        when(userServiceMock.createNewUser(createUserRequest)).thenReturn(expected);
-
         // When
         MockHttpServletResponse response = mvc.perform(post("/v1/users")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -61,12 +68,11 @@ class UserControllerTest {
                 .andReturn()
                 .getResponse();
 
-
         // Then
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.getContentAsString())
-                .isEqualTo(objectMapper.writeValueAsString(expected));
-    }
 
-    // Sad Path Time
+        Optional<UserEntity> savedUser = userRepository.findAll().stream().findFirst();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(savedUser.isPresent());
+        assertThat(savedUser.get().getEmail().equals("amelia.thompson@example.com"));
+    }
 }
